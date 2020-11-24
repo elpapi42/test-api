40d7b32eaff3acac36281a7ec4e7b729c63dc3e6
ElPapi42 <whitman-2@hotmail.com>
2020-11-24 12:53:20 -0400

    Add more validation to user model

--- api/schemas/users.py
+++ api/schemas/users.py
@@ -1,7 +1,11 @@
 from typing import Optional
 from enum import Enum
 
-from pydantic import BaseModel
+from bson.objectid import ObjectId
+from pydantic import BaseModel, validator, ValidationError
+from validator_collection import checkers
+
+from api.database import db
 
 
 class UserGender(str, Enum):
@@ -15,6 +19,27 @@ class ProfileSchema(BaseModel):
     gender: Optional[UserGender]
     document_number: Optional[str]
 
+    @validator('age')
+    def validate_age(cls, v):
+        if v <= 0 or v >= 100:
+            raise TypeError('invalid age')
+        return v
+
+class UserValidatorsModel(BaseModel):
+    """Pack the validation to the input of users models."""
+    @validator('email', check_fields=False)
+    def validate_email(cls, v):
+        if not checkers.is_email(v):
+            raise TypeError('invalid email')
+        return v
+    
+    @validator('company_id', check_fields=False)
+    def validate_company_id(cls, v):
+        count = db.companies.count_documents({'_id': ObjectId(v)})
+        if count < 1:
+            raise TypeError('supplied company is not registered')
+        return v
+
 class UserSchema(BaseModel):
     id: str
     email: str
@@ -22,13 +47,13 @@ class UserSchema(BaseModel):
     profile: Optional[ProfileSchema]
     admin: Optional[bool] = False
 
-class CreateUserSchema(BaseModel):
+class CreateUserSchema(UserValidatorsModel):
     email: str
     password: str
     company_id: Optional[str] = None
     profile: Optional[ProfileSchema]
 
-class UpdateUserSchema(BaseModel):
+class UpdateUserSchema(UserValidatorsModel):
     password: Optional[str]
     company_id: Optional[str]
     profile: Optional[ProfileSchema]
